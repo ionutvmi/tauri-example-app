4a6f20bb4e02063598552b0f82ed0f202e1ae0d0
Mihai Ionut Vilcu <ionutvmi@gmail.com>
2022-01-16 20:47:39 +0200

    Updates the styles of the layout.

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -7,6 +7,8 @@ use tauri::{CustomMenuItem, Menu, MenuItem, Submenu};
 
 
 fn main() {
+  // This is hidden by default because we disabled the window decorations/title bar
+  // from tauri.config.json
   let quit = CustomMenuItem::new("quit".to_string(), "Quit");
   let close = CustomMenuItem::new("close".to_string(), "Close");
   let submenu = Submenu::new("File", Menu::new().add_item(quit).add_item(close));
